4db95442a9ddcbe1fd9c2f6742bc2d180ebb3b17
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2021-07-18 06:57:21 +0600

    updated

--- dashboard.php
+++ dashboard.php
@@ -78,34 +78,60 @@
                                 <table id="datatablesSimple">
                                     <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
+											<th>Product ID</th>
+											<th>Product Name</th>
+											<th>Employee Name</th>
+											<th>Division</th>
+											<th>Location</th>
+											<th>Assigned Date</th>
+											<th>Action</th>
                                         </tr>
                                     </thead>
                                     <tfoot>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
+											<th>Product ID</th>
+											<th>Product Name</th>
+											<th>Employee Name</th>
+											<th>Division</th>
+											<th>Location</th>
+											<th>Assigned Date</th>
+											<th>Action</th>
                                         </tr>
                                     </tfoot>
                                     <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
-                                    </tbody>
+											<tr id="" bgcolor="#f2f2f2" class="edit_tr">
+											<?php
+											$sql	=	"select * from `product_assign` ORDER BY `id` DESC LIMIT 5";
+											$result = mysqli_query($conn, $sql);
+											while($row=mysqli_fetch_array($result))
+											{
+											
+													$product_id=$row['product_id'];
+													$sql2	=	"select * from `ams_products` where `id`='$product_id'";
+													$result2 = mysqli_query($conn, $sql2);
+													$rowp=mysqli_fetch_array($result2);
+														$employee_id	=	$row['employee_id'];
+														$sqlemp			=	"select * from `employees` where `employee_id`='$employee_id'";
+														$resultemp = mysqli_query($conn, $sqlemp);
+														$rowemp=mysqli_fetch_array($resultemp);
+													
+												?>
+												<td><span class="text"><?php echo $rowp['sl_no'] ?></span></td>
+												<td><span class="text"><?php echo $rowp['item_name'] ?></span></td>
+												<td><span class="text"><?php echo $rowemp['employee_name'] ?></span></td>
+												<td><span class="text"><?php echo $rowemp['division'] ?></span></td>
+												<td><span class="text"><?php echo $rowemp['location'] ?></span></td>
+												<td><span class="text"><?php echo $row['assign_date'] ?></span></td>
+												<td class='text-center'> 
+												<button onclick="window.location.href = 'assignqrview.php?id=<?php echo $row['id'] ?>'" class='' title="Details"> <i class="fa fa-eye text-success"></i></button>
+												<button onclick="window.location.href = 'handover-receipt.php?id=<?php echo $row['id'] ?>'" class='' title="Handover Receipt"> <i class="fa fa-chart-area text-success"></i></button>
+												</td>
+											</tr>
+
+										<?php
+										}
+										?>
+										</tbody>
                                 </table>
                             </div>
                         </div>
