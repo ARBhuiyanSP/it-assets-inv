5eed8bca2de54ef88543b10d41ea6472745f7e07
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2021-07-25 11:26:20 +0600

    updated

--- vendor-details.php
+++ vendor-details.php
@@ -2,23 +2,24 @@
 <main>
 	<div class="container-fluid px-4">
 		<!--  +++++++++++++++ Url/Breadcrumb ++++++++++++++ -->
-		<h1 class="mt-4">Dashboard</h1>
-		<ol class="breadcrumb mb-4">
-			<li class="breadcrumb-item active">Sample Page</li>
+		<ol class="breadcrumb mt-4 mb-4">
+			<li class="breadcrumb-item">Dashboard</li>
+			<li class="breadcrumb-item active">Vendor Details</li>
 		</ol>
 		<!--  +++++++++++++++ Url/Breadcrumb ++++++++++++++ -->
 		<div class="card mb-4">
 			<div class="card-header">
 				<i class="fas fa-table me-1"></i>
-				Sample Content
+				Vendor Details
 			</div>
 			<div class="card-body">
 				<div class="row justify-content-center">
-					<div class="col-md-6 mt-3 bg-info p-4 rounded">
-						<h2 class="bg-light p-2 rounded text-center text-dark">ID : <?= $id; ?></h2>
-						<h4 class="text-light">Name : <?= $name; ?></h4>
-						<h4 class="text-light">Email : <?= $address; ?></h4>
-						<h4 class="text-light">Phone : <?= $contact_person; ?></h4>
+					<div class="col-md-6 mt-3 p-4 rounded">
+						<h2 class="bg-success p-2 rounded text-center text-light">ID : <?= $vendor_id; ?></h2>
+						<h4 class="">Name : <?= $vendor_name; ?></h4>
+						<h4 class="">Address : <?= $address; ?></h4>
+						<h4 class="">Email : <?= $email; ?></h4>
+						<h4 class="">Phone : <?= $phone; ?></h4>
 						<button class="btn btn-primary" style="width:100%;"><a href="vendors.php" class="text-light">Back To Vendors List</a></button>
 					</div>
 				</div>
