090e6738b27e066a19f26f74f31642e8a8c880eb
Atiqur Rahman Bhuiyan <arbhuiyan.pits@gmail.com>
2022-11-03 16:16:36 +0600

    first commit

--- js/site_url.js
+++ js/site_url.js
@@ -1,2 +1,8 @@
-var baseUrl =   "http://localhost/it-assets-inv/";
+
+
+//baseUrl =   "http://103.54.36.11/it-assets-inv/";
+
+var getUrl = window.location;
+var baseurlOrigin = getUrl.origin; //or
+var baseUrl = baseurlOrigin+'/it-assets-inv/';
 
